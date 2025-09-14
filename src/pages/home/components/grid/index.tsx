9299c9dbdc285c11b22250fcfbffc5f5df33b00c
madina875 <evaevasmq@gmail.com>
2025-09-14 10:13:44 +0500

    first commit

---
+++ src/pages/home/components/grid/index.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const Grid = () => {
+  return (
+    <div className="Grid">
+      <h2>Grid</h2>
+    </div>
+  );
+};
+
+export default memo(Grid);
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { memo, useEffect } from "react";
+// import { api } from '../../api';
+import { api } from "../api";
+import { useDispatch } from "react-redux";
+// import { removeToken } from '../../lib/features/authSlice';
+import { removeToken } from "../lib/features/authSlice";
+
+const Account = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    api
+      .get("/auth/me")
+      .then()
+      .catch(() => {
+        dispatch(removeToken());
+      });
+  }, []);
+  return (
+    <div className="Index">
+      <h2>Account</h2>
+    </div>
+  );
+};
+
+export default memo(Account);
