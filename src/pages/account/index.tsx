81efa58165accd925d6b4c16bd22d5850d18fd04
Guli12345678 <gulsanammamadieva083@gmail.com>
2025-09-16 19:40:05 +0500

    final no more

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -7,6 +7,15 @@ import type { IUser } from "../../types";
 import { useNavigate } from "react-router-dom";
 
 const Account = () => {
+  const [isSmall, setSmall] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    const handleResize = () => setSmall(window.innerWidth < 600);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const [user, setUser] = useState<IUser | null>(null);
   const [showAddress, setShowAddress] = useState(false);
   const token = localStorage.getItem("token");
@@ -26,22 +35,22 @@ const Account = () => {
       });
   }, [token, dispatch]);
   return (
-    <div className="Index">
+    <div className="p-7 sm:p-0">
       <div className="container">
-        <h3 className="text-[44px] text-center m-10 font-semibold">
+        <h3 className="text-[24px] md:text-[44px] text-center m-10 font-semibold">
           My Account
         </h3>
-        <div className="flex gap-[102px]">
-          <div className="bg-[#F3F5F7] w-[262px] h-[498px] p-10">
-            <div className="flex relative  flex-col justify-center items-center">
-              <div className="mb-5 ">
+        <div className="flex-col sm:flex sm:flex-row sm:gap-[102px]">
+          <div className="bg-[#F3F5F7] md:w-[262px] p-10">
+            <div className="flex relative flex-col justify-center items-center">
+              <div className="mb-5">
                 <img
                   className="w-20  h-20 rounded-full object-cover"
                   src={user?.image}
                   alt=""
                 />
-                <span className="bg-black rounded-full border-2 border-white w-8 h-8 flex justify-center absolute top-[60px] right-13">
-                  <img className="w-5  " src={camera} alt="" />
+                <span className="bg-black rounded-full border-2 border-white w-8 h-8 flex justify-center absolute top-[60px] right-23 sm:right-13">
+                  <img className="w-5 " src={camera} alt="" />
                 </span>
               </div>
               <strong>
@@ -49,27 +58,59 @@ const Account = () => {
               </strong>
             </div>
             <div className="mt-12">
-              <strong onClick={() => setShowAddress(false)} className="cursor-pointer">
-                Account
-              </strong>
-              <hr className="mt-3" />
-              <div className="mt-5 leading-10 font-medium">
-                <p
-                  onClick={() => setShowAddress(!showAddress)}
-                  className="text-[#6C7275] text-[18px] cursor-pointer"
-                >
-                  Address
-                </p>
+              {isSmall === false ? (
+                <div>
+                  <strong
+                    onClick={() => setShowAddress(false)}
+                    className="cursor-pointer"
+                  >
+                    Account
+                  </strong>
+                  <hr className="mt-3" />
+                  <div className="mt-5 leading-10 font-medium hidden sm:block">
+                    <p
+                      onClick={() => setShowAddress(true)}
+                      className="text-[#6C7275] text-[18px] cursor-pointer hidden sm:block"
+                    >
+                      Address
+                    </p>
 
-                <p className="text-[#6C7275] text-[18px]">Orders</p>
-                <p className="text-[#6C7275] text-[18px]">Wishlist</p>
-                <p onClick={()=>navigate("/logout")} className="text-[#6C7275] text-[18px]">Log Out</p>
-              </div>
+                    <p className="text-[#6C7275] text-[18px]">Orders</p>
+                    <p className="text-[#6C7275] text-[18px]">Wishlist</p>
+                    <p
+                      onClick={() => navigate("/logout")}
+                      className="text-[#6C7275] text-[18px]"
+                    >
+                      Log Out
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <select
+                    className="w-full text-[#6C7275] text-[18px]"
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "account") setShowAddress(false);
+                      if (value === "address") setShowAddress(true);
+                      if (value === "logout") navigate("/logout");
+                    }}
+                  >
+                    <option value="account">Account</option>
+                    <option value="address">Address</option>
+                    <option value="orders">Orders</option>
+                    <option value="wishlist">Wishlist</option>
+                    <option value="logout">Log Out</option>
+                  </select>
+
+                  <hr className="mt-3" />
+                </div>
+              )}
             </div>
           </div>
-          <div className="flex flex-col ">
+          <div className="flex flex-col mt-20 mb-20 sm:mt-0 sm:mb-0">
             {!showAddress ? (
-              <div className="w-[707px] h-[416px]">
+              <div className="shadow-lg  md:w-[707px] p-10">
                 <h4 className="text-xl font-medium">Account Details</h4>
 
                 <br />
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo } from "react";
+import { memo, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../../lib";
 import {
@@ -11,59 +11,144 @@ import {
 const Cart = () => {
   const carts = useSelector((state: RootState) => state.cart.value);
   const dispatch = useDispatch();
-  const total = useMemo(()=> {
-    return carts.reduce((sum, item) => sum + item.price * item.quantity, 0)
-  }, [carts])
-  
+  const [shipping, setShipping] = useState(0);
+  const total = useMemo(() => {
+    return carts.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }, [carts]);
+
   return (
     <div className="container flex flex-col justify-center items-center text-center mt-20">
-      {/* <h2>Cart</h2> */}
-      {carts.length ? (
-        <div className="container grid grid-cols-2">
-          <div>
-            {carts?.map((item: ICartProduct) => (
-              <div key={item.id}>
-                <img src={item.thumbnail} width={80} alt="" />
-                <h3>{item.title}</h3>
-                <strong>{item.stock}</strong>
-                <p>
-                  {item.price} * {item.quantity} = {item.price * item.quantity}
-                </p>
-                <div>
-                  <button
-                    className="disabled:opacity-30 p-2"
-                    disabled={item.quantity <= 1}
-                    onClick={() => dispatch(decreaseAmount(item))}
-                  >
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button
-                    disabled={item.quantity >= item.stock}
-                    className=" p-2"
-                    onClick={() => dispatch(increaseAmount(item))}
-                  >
-                    +
-                  </button>
-                  <button onClick={() => dispatch(removeFromCart(item))}>
-                    delete
-                  </button>
-                </div>
-              </div>
-            ))}
+      <h2 className="text-[54px] font-medium">Cart</h2>
+
+      <div className="sm:flex gap-20 hidden sm:block w-[900px] mt-10 justify-around">
+        <div>
+          <div className="flex gap-5">
+            <p className="bg-black text-white w-[42px] h-[42px] rounded-full flex justify-center items-center">
+              1
+            </p>
+            <p className="mt-2 text-[16px]">Shopping cart</p>
           </div>
+          <hr className="mt-5 h-0.5 w-[200px] bg-black border-none" />
+        </div>
+        <div className="flex gap-5">
+          <p className="bg-slate-300 text-white w-[42px] h-[42px] rounded-full flex justify-center items-center">
+            2
+          </p>
+          <p className="mt-2 text-[16px]">Checkout details</p>
+        </div>
+        <div className="flex gap-5">
+          <p className="bg-slate-300 text-white w-[42px] h-[42px] rounded-full flex justify-center items-center">
+            3
+          </p>
+          <p className="mt-2 text-[16px]">Order complete</p>
+        </div>
+      </div>
+
+      {carts.length ? (
+        <div className="container grid grid-cols-1 sm:grid-cols-2 mt-10">
           <div>
-            <h2>Total : {Math.floor(total).toLocaleString()} USD</h2>
+            <table className="w-full text-left border-collapse">
+              <thead>
+                <tr className="border-b">
+                  <th className="p-4 font-semibold">Product</th>
+                </tr>
+              </thead>
+              <tbody>
+                {carts.map((item: ICartProduct) => (
+                  <tr key={item.id} className="border-b p-10">
+                    <td className="p-4">
+                      <div className=" sm:flex flex flex-row items-center gap-4">
+                        <img
+                          src={item.thumbnail}
+                          alt={item.title}
+                          className="w-16 h-16 object-cover"
+                        />
+                        <div className="flex flex-col">
+                          <span>{item.title}</span>
+                          <button
+                            onClick={() => dispatch(removeFromCart(item))}
+                            className=" text-red-700 mt-1"
+                          >
+                            &#10005; remove
+                          </button>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="p-4">
+                      <div className="flex items-center gap-2">
+                        <button
+                          disabled={item.quantity <= 1}
+                          onClick={() => dispatch(decreaseAmount(item))}
+                          className="px-2 py-1 border rounded disabled:opacity-30"
+                        >
+                          -
+                        </button>
+                        <span>{item.quantity}</span>
+                        <button
+                          disabled={item.quantity >= item.stock}
+                          onClick={() => dispatch(increaseAmount(item))}
+                          className="px-2 py-1 border rounded disabled:opacity-30"
+                        >
+                          +
+                        </button>
+                      </div>
+                    </td>
+                    <td className="p-4">${item.price}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="mt-10 border p-6 rounded-lg shadow-md w-full max-w-md mb-10 ml-auto">
+            <h2 className="text-xl font-bold mb-4">Cart Summary</h2>
+            <div className="space-y-2 text-gray-700">
+              <label className="flex justify-center items-center gap-2">
+                <input
+                  value="0"
+                  onChange={() => setShipping(0)}
+                  type="radio"
+                  name="shipping"
+                />
+                Free shipping $0.00
+              </label>
+              <label className="flex justify-center items-center gap-2">
+                <input
+                  value="15"
+                  onChange={() => setShipping(15)}
+                  type="radio"
+                  name="shipping"
+                />
+                Express Shipping: + $15.00
+              </label>
+              <label className="flex justify-center items-center gap-2">
+                <input
+                  value="21"
+                  onChange={() => setShipping(21)}
+                  type="radio"
+                  name="shipping"
+                />
+                Pick Up: $21.00
+              </label>
+            </div>
+
+            <div className="text-lg font-semibold mt-5">
+              <p>Subtotal: $ {total.toFixed(2)}</p>
+              <hr className="my-4 h-[2px] bg-gray-300 border-none" />
+              <p>Total: $ {(total + shipping).toFixed(2)}</p>{" "}
+            </div>
+            <button className="mt-6 w-full bg-black text-white py-2 rounded hover:bg-gray-800">
+              Checkout
+            </button>
           </div>
         </div>
       ) : (
-        <div className="flex flex-col justify-around">
+        <div className="flex flex-col justify-around items-center mt-10">
           <img
             src="https://uzum.uz/static/img/shopocat.490a4a1.png"
             width={150}
             alt=""
           />
-          <p className="text-3xl font-sans">empty</p>
+          <p className="text-3xl font-sans font-medium mt-4">empty</p>
         </div>
       )}
     </div>
