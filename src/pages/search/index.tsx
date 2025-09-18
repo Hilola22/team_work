051786bb01d102dea4d7b58d432a80edf631392f
madina875 <evaevasmq@gmail.com>
2025-09-18 17:01:25 +0500

    search

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,25 +1,40 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import ProductView from "../../components/product-view/ProductView";
-import { useFetch } from "../../hooks/useFetch";
 
 const Search = () => {
-  const { data } = useFetch("/products", { limit: 3 });
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState<any>("");
+  console.log(data);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    fetch(`https://dummyjson.com/products/search?q=${search}`)
+      .then((res) => res.json())
+      .then((data) => setData(data.products));
+  };
 
   return (
     <div className="container my-[20px] pb-[20px]">
-      <div className="h-[60px] bg-gray-100 rounded-2xl flex items-center gap-[20px] justify-between px-[40px] mb-[100px]">
+      <form
+        onSubmit={handleSubmit}
+        className="h-[60px] bg-gray-100 rounded-2xl flex items-center gap-[20px] justify-between px-[40px] mb-[100px]"
+      >
         <input
           className="w-[100%] h-[100%] text-2xl hover:outline-0 outline-0"
           type="text"
           placeholder="search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <button className="bg-black px-[40px] text-white text-[20px] py-[6px] rounded-[15px]">
+        <button className="bg-black px-[40px] text-white text-[20px] py-[6px] rounded-[15px] hover:text-green-200">
           send
         </button>
-      </div>
-      <div>
-        <ProductView data={data?.products} />
-      </div>
+      </form>
+      {search ? (
+        <ProductView data={data} />
+      ) : (
+        <h3 className="text-center text-2xl">search...</h3>
+      )}
     </div>
   );
 };
